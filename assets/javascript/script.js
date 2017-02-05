66efbe37ce05dc9f27a3d81db20c07d70ce59566
Eony5 <jonathancodas@gmail.com>
2017-02-06 00:05:48 +0100

    Rajouté plus de swag

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,54 @@
 window.onload = function(){
+  var delete_button = document.createElement("div");
+  delete_button.id = "delete-button";
+  delete_button.innerHTML = "Delete";
+
   var projects_area = document.querySelector("#projects");
   var add_project = document.querySelector("#add-project");
+  var add_task_buttons = document.querySelectorAll(".add-task");
+  var tasks = document.querySelectorAll(".task");
 
+  /* TRIGGERS */
+
+  // Add project button
   add_project.onclick = function(){
+    addProject();
+  };
+
+  // Add task buttons on each project
+  function setAddtaskTriggers(){
+    add_task_buttons = document.querySelectorAll(".add-task");
+    for(var i = 0; i < add_task_buttons.length; i++){
+      add_task_buttons[i].onclick = function(){
+        addTask(this);
+      }
+    }
+  }
+  setAddtaskTriggers();
+
+  // On hover task
+  for(var i = 0; i < tasks.length; i++){
+    tasks[i].onmouseover = function(){
+      this.appendChild(delete_button);
+      delete_button.className = "delete-button animate-del-button";
+    }
+    tasks[i].onmouseout = function(){
+      if(getCurrentHoverId() !== "delete-button" && getCurrentHoverId() !== this.id){
+        this.removeChild(delete_button);
+      }
+    }
+  }
+
+  delete_button.onclick = function(){
+    this.parentElement.parentElement.removeChild(this.parentElement);
+  }
+
+  /* FUNCTIONS */
+  function getCurrentHoverId(){
+    var hovered = document.querySelectorAll(":hover");
+    return hovered[hovered.length-1].id;
+  }
+  function addProject(){
     var created_project = document.createElement("section");
     var created_title = document.createElement("h1");
     created_title.innerHTML = "New project";
@@ -15,5 +61,15 @@ window.onload = function(){
     created_project.className = "project animate-add";
 
     projects_area.appendChild(created_project);
-  };
+    setAddtaskTriggers();
+  }
+  function addTask(e){
+    console.log("Added task");
+    var created_task = document.createElement("div");
+    created_task.className = "task animate-add";
+    created_task.innerHTML = "New task";
+
+    e.parentElement.appendChild(created_task);
+    tasks = document.querySelectorAll(".task");
+  }
 };
